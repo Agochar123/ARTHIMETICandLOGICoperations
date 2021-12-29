ba1e6d4946d3a8581c595f7f0708c07d6a83cd9e
Agochar123 <94464716+Agochar123@users.noreply.github.com>
2021-12-29 19:32:54 +0530

    Update Student_Project.py

--- Student_Project.py
+++ Student_Project.py
@@ -1,1 +1,17 @@
-#Write the code over here
+n1=int(input("enter the value of 1st no."))
+n2=int(input("enter the value of 2nd no."))
+
+def multiplication(n1,n2):
+    print("multiplication of both numbers is",n1*n2)
+def division(n1,n2):
+    print("division of both numbers is",n1/n2)    
+def addition(n1,n2):
+    print("addition of both numbers is",n1+n2)
+def subraction(n1,n2):
+    print("subraction of both numbers is",n1-n2) 
+multiplication(n1,n2)
+division(n1,n2)
+addition(n1,n2)
+subraction(n1,n2)
+
+
